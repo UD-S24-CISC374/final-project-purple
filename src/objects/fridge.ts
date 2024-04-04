6908123e0bdaf027c0147c2e4614adb7eecbb5f5
Mikey <michael.j.bocelli@gmail.com>
2024-04-04 18:15:24 -0400

    fix: lint error

--- src/objects/fridge.ts
+++ src/objects/fridge.ts
@@ -1,27 +1,23 @@
 import Phaser from "phaser";
 
-
 export default class Fridge extends Phaser.GameObjects.Zone {
-    
     public inside: Phaser.GameObjects.Sprite;
     tmpInside: Phaser.GameObjects.Rectangle;
 
-    constructor(
-        scene: Phaser.Scene,
-        x: number,
-        y: number,
-    ) {
+    constructor(scene: Phaser.Scene, x: number, y: number) {
         super(scene, x, y, 190, 320);
-        this.setInteractive()
-        .on("pointerdown", this.click)
-        this.tmpInside = scene.add.rectangle(250, 445, 250, 400, 0xc0c0c0).setAlpha(0);
+        this.setInteractive().on("pointerdown", this.click);
+        this.tmpInside = scene.add
+            .rectangle(250, 445, 250, 400, 0xc0c0c0)
+            .setAlpha(0);
         scene.add.rectangle(x, y, 155, 200, 0xff0000).setAlpha(0.4);
         scene.add.existing(this);
     }
 
-    click(pointer: Phaser.Input.Pointer){
-        console.log("hey michyyyy") 
+    click() {
         //this.tmpInside.setAlpha(1);
-        this.tmpInside.alpha > 0 ? this.tmpInside.setAlpha(0) : this.tmpInside.setAlpha(1);
+        this.tmpInside.alpha > 0
+            ? this.tmpInside.setAlpha(0)
+            : this.tmpInside.setAlpha(1);
     }
 }
