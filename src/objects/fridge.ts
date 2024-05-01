56ef5233a8034a2d6b32c25f6edec2672b5b7ded
Farhan Ohe <farhanto@udel.edu>
2024-05-01 17:25:39 -0400

    modified pantry and fridge

--- src/objects/fridge.ts
+++ src/objects/fridge.ts
@@ -3,59 +3,66 @@ import Ingredient from "./ingredient";
 
 export default class Fridge extends Phaser.GameObjects.Zone {
     public inside: Phaser.GameObjects.Image;
-    sprites: Phaser.GameObjects.Sprite[] = new Array<Phaser.GameObjects.Sprite>(
-        6
-    );
+    sprites: Phaser.GameObjects.Sprite[] = [];
 
     constructor(scene: Phaser.Scene, x: number, y: number) {
         super(scene, x, y, 150, 270);
-        this.setInteractive().on("pointerdown", this.click);
+        this.setInteractive().on("pointerdown", this.click.bind(this));
         this.inside = scene.add.image(250, 450, "fridge-inside").setAlpha(0);
         scene.add.zone(x, y, 155, 200);
         scene.add.existing(this);
-        this.sprites[0] = scene.add
-            .sprite(171, 375, "milk")
-            .setInteractive()
-            .setVisible(false)
-            .setScale(0.2, 0.2)
-            .on("pointerdown", () => {
-                this.spawnIngredeint("milk");
+
+        const ingredients = [
+            { name: "milk", x: 171, y: 375 },
+            { name: "butter", x: 328, y: 380 },
+            { name: "chicken", x: 250, y: 520 },
+        ];
+        ingredients.forEach((ingredient) => {
+            const sprite = scene.add
+                .sprite(ingredient.x, ingredient.y, ingredient.name)
+                .setInteractive()
+                .setVisible(false)
+                .setScale(0.2, 0.2)
+                .on("pointerdown", (pointer: { x: number; y: number }) => {
+                    this.spawnIngredient(ingredient.name, pointer.x, pointer.y);
+                });
+            this.sprites.push(sprite);
+
+            scene.input.setDraggable(sprite);
+            sprite.on("drag", (dragX: number, dragY: number) => {
+                sprite.x = dragX;
+                sprite.y = dragY;
             });
-        this.sprites[1] = scene.add
-            .sprite(328, 380, "butter")
-            .setInteractive()
-            .setVisible(false)
-            .setScale(0.2, 0.2)
-            .on("pointerdown", () => {
-                this.spawnIngredeint("butter");
-            });
-        this.sprites[2] = scene.add
-            .sprite(250, 520, "chicken")
-            .setInteractive()
-            .setVisible(false)
-            .setScale(0.2, 0.2)
-            .on("pointerdown", () => {
-                this.spawnIngredeint("chicken");
-            });
+        });
     }
 
-    spawnIngredeint(ingrdName: string) {
-        new Ingredient(
+    spawnIngredient(ingrdName: string, x: number, y: number) {
+        const ingredient = new Ingredient(
             this.scene,
-            this.scene.cameras.main.width - 200,
-            this.scene.cameras.main.centerY,
+            x,
+            y,
             ingrdName,
             ingrdName
         );
+        this.closeFridge();
+        return ingredient;
     }
 
     click() {
         if (this.inside.alpha > 0) {
-            this.inside.setAlpha(0);
-            this.sprites.map((sprite) => sprite.setVisible(false));
+            this.closeFridge();
         } else {
-            this.inside.setAlpha(1);
-            this.sprites.map((sprite) => sprite.setVisible(true));
+            this.openFridge();
         }
+    }
+
+    closeFridge() {
+        this.inside.setAlpha(0);
+        this.sprites.forEach((sprite) => sprite.setVisible(false));
+    }
+
+    openFridge() {
+        this.inside.setAlpha(1);
+        this.sprites.forEach((sprite) => sprite.setVisible(true));
     }
 }
--- src/objects/pantry.ts
+++ src/objects/pantry.ts
@@ -3,43 +3,62 @@ import Ingredient from "./ingredient";
 
 export default class Pantry extends Phaser.GameObjects.Zone {
     public inside: Phaser.GameObjects.Image;
-    sprites: Phaser.GameObjects.Sprite[] = new Array<Phaser.GameObjects.Sprite>(
-        6
-    );
+    sprites: Phaser.GameObjects.Sprite[] = [];
 
     constructor(scene: Phaser.Scene, x: number, y: number) {
         super(scene, x, y, 150, 270);
-        this.setInteractive().on("pointerdown", this.click);
+        this.setInteractive().on("pointerdown", this.click.bind(this));
         this.inside = scene.add.image(250, 450, "pantry-inside").setAlpha(0);
         scene.add.zone(x, y, 155, 200);
         scene.add.existing(this);
-        this.sprites[0] = scene.add
-            .sprite(160, 380, "carrot")
-            .setInteractive()
-            .setVisible(false)
-            .setScale(0.2, 0.2)
-            .on("pointerdown", () => {
-                this.spawnIngredeint("carrot");
+
+        const ingredients = [{ name: "carrot", x: 160, y: 380 }];
+        ingredients.forEach((ingredient) => {
+            const sprite = scene.add
+                .sprite(ingredient.x, ingredient.y, ingredient.name)
+                .setInteractive()
+                .setVisible(false)
+                .setScale(0.2, 0.2)
+                .on("pointerdown", (pointer: { x: number; y: number }) => {
+                    this.spawnIngredient(ingredient.name, pointer.x, pointer.y);
+                });
+            this.sprites.push(sprite);
+
+            scene.input.setDraggable(sprite);
+            sprite.on("drag", (dragX: number, dragY: number) => {
+                sprite.x = dragX;
+                sprite.y = dragY;
             });
+        });
     }
 
-    spawnIngredeint(ingrdName: string) {
-        new Ingredient(
+    spawnIngredient(ingrdName: string, x: number, y: number) {
+        const ingredient = new Ingredient(
             this.scene,
-            this.scene.cameras.main.width - 200,
-            this.scene.cameras.main.centerY,
+            x,
+            y,
             ingrdName,
             ingrdName
         );
+        this.closePantry();
+        return ingredient;
     }
 
     click() {
         if (this.inside.alpha > 0) {
-            this.inside.setAlpha(0);
-            this.sprites.map((sprite) => sprite.setVisible(false));
+            this.closePantry();
         } else {
-            this.inside.setAlpha(1);
-            this.sprites.map((sprite) => sprite.setVisible(true));
+            this.openPantry();
         }
+    }
+
+    closePantry() {
+        this.inside.setAlpha(0);
+        this.sprites.forEach((sprite) => sprite.setVisible(false));
+    }
+
+    openPantry() {
+        this.inside.setAlpha(1);
+        this.sprites.forEach((sprite) => sprite.setVisible(true));
     }
 }
