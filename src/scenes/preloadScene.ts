b5859403970d34de8a70e44e0fef3e3ac754a474
Mikey <michael.j.bocelli@gmail.com>
2024-04-30 21:12:07 -0400

    reset preload starter scene to menu

--- src/scenes/preloadScene.ts
+++ src/scenes/preloadScene.ts
@@ -58,6 +58,6 @@ export default class PreloadScene extends Phaser.Scene {
             frameRate: 20,
             repeat: 0,
         });
-        this.scene.start("Shift1");
+        this.scene.start("MainMenu");
     }
 }
