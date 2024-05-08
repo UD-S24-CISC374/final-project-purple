8d446ff7357339b4555f5dad0881bc9b248d5609
Farhan Ohe <farhanto@udel.edu>
2024-05-08 11:07:44 -0400

    making changes to fridge and ingredients

--- src/objects/fridge.ts
+++ src/objects/fridge.ts
@@ -3,9 +3,8 @@ import Ingredient from "./ingredient";
 
 export default class Fridge extends Phaser.GameObjects.Zone {
     public inside: Phaser.GameObjects.Image;
-    sprites: Phaser.GameObjects.Sprite[] = [];
+    ingredientSprites: Ingredient[] = [];
     ingredients = [
-        // Now a class property
         { name: "milk", x: 171, y: 375 },
         { name: "butter", x: 328, y: 380 },
         { name: "chicken", x: 250, y: 520 },
@@ -18,45 +17,19 @@ export default class Fridge extends Phaser.GameObjects.Zone {
         scene.add.zone(x, y, 155, 200);
         scene.add.existing(this);
 
+        // Instantiate each ingredient as an Ingredient object
         this.ingredients.forEach((ingredient) => {
-            const sprite = scene.add
-                .sprite(ingredient.x, ingredient.y, ingredient.name)
-                .setInteractive()
-                .setVisible(false)
-                .setScale(0.2, 0.2)
-                .on("pointerover", () => {
-                    sprite.setScale(0.3, 0.24);
-                })
-                .on("pointerout", () => {
-                    sprite.setScale(0.2, 0.2);
-                })
-                .on("pointerdown", (pointer: { x: number; y: number }) => {
-                    this.spawnIngredient(ingredient.name, pointer.x, pointer.y);
-                });
-            this.sprites.push(sprite);
-
-            scene.input.setDraggable(sprite);
-            sprite
-                .on("drag", (dragX: number, dragY: number) => {
-                    sprite.x = dragX;
-                    sprite.y = dragY;
-                })
-                .on("dragend", () => {
-                    this.resetIngredients();
-                });
+            const ingredientSprite = new Ingredient(
+                scene,
+                ingredient.x,
+                ingredient.y,
+                ingredient.name,
+                ingredient.name // Assuming the image is named the same as the ingredient
+            );
+            ingredientSprite.setVisible(false); // Initially invisible
+            this.ingredientSprites.push(ingredientSprite);
+            this.scene.input.setDraggable(ingredientSprite);
         });
-    }
-
-    spawnIngredient(ingrdName: string, x: number, y: number) {
-        const ingredient = new Ingredient(
-            this.scene,
-            x,
-            y,
-            ingrdName,
-            ingrdName
-        );
-        this.closeFridge();
-        return ingredient;
     }
 
     click() {
@@ -67,19 +40,13 @@ export default class Fridge extends Phaser.GameObjects.Zone {
         }
     }
 
-    closeFridge() {
-        this.inside.setAlpha(0);
-        this.sprites.forEach((sprite) => sprite.setVisible(false));
-    }
-
     openFridge() {
         this.inside.setAlpha(1);
-        this.sprites.forEach((sprite) => sprite.setVisible(true));
+        this.ingredientSprites.forEach((sprite) => sprite.setVisible(true));
     }
-    resetIngredients() {
-        this.sprites.forEach((sprite, index) => {
-            sprite.x = this.ingredients[index].x;
-            sprite.y = this.ingredients[index].y;
-        });
+
+    closeFridge() {
+        this.inside.setAlpha(0);
+        this.ingredientSprites.forEach((sprite) => sprite.setVisible(true));
     }
 }
