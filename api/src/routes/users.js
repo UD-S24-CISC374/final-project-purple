2f5aeeab7ccd8be787343abbbd5a2f397128c5c6
Mikey <michael.j.bocelli@gmail.com>
2024-05-17 01:15:54 -0400

    feat: route to grab top 10 players

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -11,7 +11,25 @@ connDb().then((_db) => {
 router.get("/", async (req, res) => {
     let collection = await db.collection("users");
     let results = await collection.find({}).toArray();
-    res.send(results);
+
+    res.send(results).status(200);
+});
+
+// get top 10 players with highest profit
+router.get("/leaderboard", async (req, res) => {
+    try {
+        let collection = await db.collection("users");
+        let results = await collection
+            .aggregate([
+                { $project: { name: 1, best_profit: 1 } },
+                { $sort: { best_profit: -1 } },
+                { $limit: 10 },
+            ])
+            .toArray();
+        res.send(results).status(200);
+    } catch {
+        res.send("FAILED TO LOAD").status(500);
+    }
 });
 
 module.exports = router;
